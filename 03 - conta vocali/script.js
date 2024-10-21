0a1096c6239cd4aa87c02579899111417d1d59ee
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-21 14:34:01 +0200

    ADD 3rd exercise

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,25 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+function vowelCount(text) {
+    const vowels = ['a','e','i','o','u','y']
+    let numberVowels = 0
+    let textVowels = []
 
+    const lowerCaseText = text.toLowerCase()
+    for (let i = 0; i < lowerCaseText.length; i++) {
+        if (vowels.includes(lowerCaseText[i])){
+            numberVowels++
+            textVowels.push(lowerCaseText.charAt(i))
+        }
+    }
+    return (textVowels)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+vowelCount(word)
+console.log(vowelCount(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
